7dc47e1de095f1765e55b9adfe625262066e2d35
ficarra_gabriele <ficarra.gabriele@franchettisalviani.net>
2025-02-18 12:00:23 +0100

    parte 1

---
+++ Es G/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int[,] m1 = { {0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0}  };
+
+for (int i = 0; i < m1.GetLength(0); i++)
+{
+    for (int j = 0; j < m1.GetLength(0); j++)
+    {
+        Random rnd = new Random.Next(0, 5);
+    }
+}
