96208b31ba4f3979a880450b4ae51402ef4e3267
ficarra_gabriele <ficarra.gabriele@franchettisalviani.net>
2025-02-18 12:22:29 +0100

    parte 2

--- Es G/Program.cs
+++ Es G/Program.cs
@@ -8,8 +8,16 @@ int[,] m1 = { {0, 0, 0, 0, 0},
 
 for (int i = 0; i < m1.GetLength(0); i++)
 {
+    Random rnd = new Random();
+    int n = rnd.Next(0, 5);
+
     for (int j = 0; j < m1.GetLength(0); j++)
     {
-        Random rnd = new Random.Next(0, 5);
+        if (m1[i, n] == 0)
+        {
+            m1[i, n] = -1;
+        }
+        Console.Write("[" + m1[i, j] + "]");
     }
+    Console.WriteLine();
 }
